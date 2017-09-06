a12d6dc03c99e5ba0207e2fd183c80dedf90df22
scaramallion <scaram@gmail.com>
2017-09-06 16:04:59 +1000

    Update Travis #17

--- manage.py
+++ manage.py
@@ -6,5 +6,5 @@ import sys
 from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pdbook.tests.test_settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pdbook.tests.settings")
     execute_from_command_line(sys.argv)
